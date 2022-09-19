7f88bf0819b2fb466049165b87ca3727e1b4d002
William_Rose <william.rose@modjoul.com>
2022-09-19 10:41:20 -0400

    Basic skeleton

--- AWS_BOTO3_hosting.py
+++ AWS_BOTO3_hosting.py
@@ -1,31 +1,46 @@
 import json
-import smtplib
+import boto3
 import pandas as pd
 
-sender_email = "william.rose@modjoul.com"
-rec_email = "william.rose@modjoul.com"
-password = ""
-
-
+# This is where the API call will happen
+# For now I am using hardcoded data
 with open('data.json') as data_1:
     read_content_1 = json.load(data_1)
-
-
-
 devices = read_content_1['devices']
+
 offline_devices = []
+offline_devices_dictionaries = []
+
+# Filter down to be a list of dictionaries
 for device in devices:
-
     if device['online'] == '0':
-        offline_device_name = device
-        offline_devices.append(offline_device_name)
-
-df=pd.DataFrame(offline_devices)
-df.to_csv('test_1.csv')
+        offline_devices.append(device)
 
 
+# Filter out keys in the dictionaries that that match in include_keys
+include_keys = ['name', 'deviceid', 'lastseen']
+for i in offline_devices:
+        newdictionary = {}
+        for k,v in i.items():
+            if k in include_keys:
+                newdictionary[k] = v
+        offline_devices_dictionaries.append(newdictionary)
 
-print(len(offline_devices))
+print(offline_devices_dictionaries)
+
+# Format to pandas dataframe
+df=pd.DataFrame(offline_devices_dictionaries)
+df.to_csv('sliced_data.csv')
+
+# Upload to s3 bucket
+s3 = boto3.resource('s3')
+s3.meta.client.upload_file()
+
+
+
+
+
+
 
     
 
